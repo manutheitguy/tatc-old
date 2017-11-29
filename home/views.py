3611ef881cbabfaa2296d4632e945f9a2d846016
Emmanuel Makonde <manutheitguy@gmail.com>
2017-11-29 16:06:51 +0300

    Initial Commit

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+
+    return render(request, 'home/index.html')
+
+def about(request):
+
+    return render(request, 'home/about.html')
+
+def services(request):
+
+    return render(request, 'home/services.html')
